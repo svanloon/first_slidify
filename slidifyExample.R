c981139cd46c0b5c82203a4557e3e5c91a1b4ec5
Steve Van Loon <steve@pointdrive.com>
2015-03-29 00:21:11 -0500

    publishing deck

--- slidifyExample.R
+++ slidifyExample.R
@@ -2,3 +2,10 @@ setwd("project")
 library(devtools)
 library(slidify)
 author("first_slidify")
+
+slidify("index.Rmd")
+library(knitr)
+browseURL("index.html")
+
+publish_github("first_slidify", username=getOption("svanloon"))
+
